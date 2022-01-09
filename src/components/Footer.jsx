10a290c399a4a9e49182592718b293189586c7f2
Aleksandar Sokolovic <78912243+School199@users.noreply.github.com>
2022-01-09 12:45:16 +0100

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-const date = new Date().getFullYear();
-
 function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright © {date}</p>
+      <p>Copyright ⓒ {year}</p>
     </footer>
   );
 }
